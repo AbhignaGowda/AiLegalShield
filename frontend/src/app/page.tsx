21ad5ecf386d1aef2330c3513f7d4dc10f6f3b68
Abhigna <abhignagowda07@gmail.com>
2025-08-18 17:59:41 +0530

    addded modern hero section using react bits

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from '@/components/Hero';
 
 const page = () => {
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <Hero/>
+    </>
   )
 }
 
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Beams from './Beams';
+const Hero = () => {
+  return (
+    <div>
+
+<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <Beams
+    beamWidth={2}
+    beamHeight={15}
+    beamNumber={12}
+    lightColor="#ffffff"
+    speed={2}
+    noiseIntensity={1.75}
+    scale={0.2}
+    rotation={0}
+  />
+</div>
+    </div>
+  )
+}
+
+export default Hero
